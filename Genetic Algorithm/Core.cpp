7ab1bf4c5f385a2f625801d9762bcd8d40cd7803
Taehwan Kim <taehwan642@naver.com>
2020-03-26 20:51:03 +0900

    Genetic Algorithm

--- Genetic Algorithm/Core.cpp
+++ Genetic Algorithm/Core.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <vector>
 #include <list>
+
 using namespace std;
 /* UPDATES
 	유전 알고리즘
@@ -11,14 +12,14 @@ UPDATES */
 
 
 // individual 의 수
-#define population 20
+#define population 50
 // 각 chromosome의 유전자의 수
 #define genes 8
 // 상위 부모의 수 (순위 기반 선택)
-#define top 5
+#define top 15
 
 list<class individual> individuals;
-list<class individual> top_individuals;
+vector<class individual> top_individuals;
 
 int Random(int start, int end)
 {
@@ -51,7 +52,6 @@ int GetFitness(individual* in)
 individual Single_Point_Crossover(individual* a, individual* b)
 {
 	int r = Random(0, genes - 1);
-	cout << r << endl;
 	individual child;
 	for (int i = 0; i < genes; i++)
 	{
@@ -69,9 +69,9 @@ individual Single_Point_Crossover(individual* a, individual* b)
 // 선택 (Selection) OK
 // 교차 (Crossover) OK, 하지만 더 추가해야함
 // 변이 (Mutation)
-// 대치 (Replace) 
-// 반복 (Loop)
-// 종료 (Quit)
+// 대치 (Replace) OK
+// 반복 (Loop) OK
+// 종료 (Quit) OK
 void Initalize()
 {
 	for (int start = 0; start < population; start++)
@@ -79,7 +79,7 @@ void Initalize()
 		individual ind;
 		for (int i = 0; i < genes; i++)
 		{
-			ind.chromosome[i] = Random(0, 9);
+			ind.chromosome[i] = Random(0, 1); // 바꿔야함
 		}
 		GetFitness(&ind);
 		individuals.push_back(ind);
@@ -101,6 +101,38 @@ void Selection()
 
 void Crossover()
 {
+	individuals.clear();
+	for (int start = 0; start < population; start++)
+	{
+		individual ind;
+		int x = Random(0, top - 1);
+		int y = Random(0, top - 1);
+		ind = Single_Point_Crossover(&top_individuals[x], &top_individuals[y]);
+		GetFitness(&ind);
+		individuals.push_back(ind);
+		individuals.sort(Comp);
+	}
+}
+
+void Mutation()
+{
+	int m = Random(0, 100);
+	if (m == 0)
+	{
+		int a = Random(0, population - 1);
+		int b = Random(0, genes - 1);
+		int c = Random(0, 1); // 바꿔야함
+		int num = 0;
+		for (auto it : individuals)
+		{
+			if (num == a)
+			{
+				it.chromosome[b] = c;
+				return;
+			}
+			num++;
+		}
+	}
 
 }
 
@@ -108,11 +140,44 @@ void Reset()
 {
 	top_individuals.clear();
 }
+
+void Print()
+{
+	list<individual>::iterator iter = individuals.begin();
+	for (iter; iter != individuals.end(); iter++)
+	{
+		for (int i = 0; i < genes; i++)
+		{
+			cout << iter->chromosome[i];
+		}
+		printf(" [ %d ] \t", iter->fitness);
+		cout << endl;
+	}
+}
+
 int main(void)
 {
 	srand(time(0));
 	Initalize();
-	Selection();
+	Print();
+	int age = 1;
+	cout << age << " 세대" << endl;
+	while (true)
+	{
+		int num = 0;
+		scanf_s("%d", &num);
+		for (int i = 0; i < num; i++)
+		{
+			system("cls");
+			Reset();
+			Selection();
+			Crossover();
+			Mutation();
+			Print();
+			age++;
+			cout << age << " 세대" << endl;
+		}
+	}
 	
 	return 0;
 }
